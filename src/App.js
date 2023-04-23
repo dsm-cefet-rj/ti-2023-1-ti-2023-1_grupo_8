d8084a08c6675c44ea722425a611d0ac7014646f
RicardoFAraujo <128547223+RicardoFAraujo@users.noreply.github.com>
2023-04-23 03:01:49 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,36 +1,57 @@
+import React, { useState } from "react";
+import "./App.css";
+import Mesas from "./componentes/Mesas";
+import AddMesa from "./componentes/AddMesa";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import MesaDetails from "./componentes/MesaDetails";
+import EditMesa from "./componentes/EditMesa";
 
-import './App.css';
-
-import React, { useState } from 'react';
-import ListaMesas from './componentes/ListaMesas/ListaMesas';
-import Form from './componentes/Form/Form';
-
-import Banner from './componentes/Banner';
-//import './Restaurante.css';
-
-function App() {
+const App = () => {
   const [mesas, setMesas] = useState([
-    { numero: 1, cadeiras: 4, ocupada: false },
-    { numero: 2, cadeiras: 6, ocupada: true },
-    { numero: 3, cadeiras: 2, ocupada: false },
-    { numero: 4, cadeiras: 2, ocupada: true },
+    { id: 1, nome: "Mesa 1", lugares: 4, ocupada: true, garcom: "João", pedido: [] },
+    { id: 2, nome: "Mesa 2", lugares: 6, ocupada: false, garcom: "Maria", pedido: [] },
+    { id: 3, nome: "Mesa 3", lugares: 8, ocupada: true, garcom: "José", pedido: [] },
+    { id: 4, nome: "Mesa 4", lugares: 10, ocupada: false, garcom: "Joana", pedido: [] },
   ]);
 
-  const addMesa = (cadeiras) => {
-    const numero = mesas.length + 1;
-    const ocupada = false;
-    const novaMesa = { numero, cadeiras, ocupada };
-    setMesas([...mesas, novaMesa]);//operado spread - copia o array original e adiciona a nova mesa no final
+  const handleMesaAddition = (mesaId) => {
+    const newMesas = [...mesas, {
+      id: mesaId,
+      ocupada: false
+    }];
+
+    setMesas(newMesas);
+  };
+
+  const handleMesaEdit = (mesaEditada) => {
+    const mesaIndex = mesas.findIndex(m => m.id === mesaEditada.id);
+    const novasMesas = [...mesas];
+    novasMesas[mesaIndex] = mesaEditada;
+    setMesas(novasMesas);
   };
 
   return (
-    <div className="app">
-      <Banner/>
-      <h1>Mesas</h1>
-      <ListaMesas mesas={mesas} />
-      <Form onSubmit={addMesa} />
-    </div>
+    <Router>
+      <div className="container">
+        <Route path="/" exact render={() => (
+          <>
+            <AddMesa handleMesaAddition={handleMesaAddition} />
+            <Mesas mesas={mesas} />
+          </>
+        )} />
+        <Route path="/mesa/:id" exact render={({ match }) => (
+          <MesaDetails mesa={mesas.find(m => m.id === parseInt(match.params.id))} handleMesaEdit={handleMesaEdit} />
+        )} />
+        <Route path="/mesa/:id/editar" exact render={({ match }) => (
+          <EditMesa mesa={mesas.find(m => m.id === parseInt(match.params.id))} handleMesaEdit={handleMesaEdit} />
+        )} />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
+
+//<Route path="/mesa/:id" exact component={MesaDetails} />
