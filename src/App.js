efdd6b5432c012968b9b531e0d2aae9ea7a5392a
RicardoFAraujo <128547223+RicardoFAraujo@users.noreply.github.com>
2023-04-25 19:04:18 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -7,6 +7,52 @@ import AddMesa from "./pages/AddMesa";
 import EditMesas from "./pages/EditMesas";
 import DetailsMesa from "./pages/DetailsMesa";
 import Cozinha from "./pages/Cozinha";
+import React, { useState } from 'react';
+import Pedidos from './componentes/Pedidos';
+const pedidos = [
+  {
+    id: 1,
+    mesa: 1,
+    valor: 50.0,
+    itens: [
+      { id: 1, nome: 'Hamburguer', valor: 25.0 },
+      { id: 2, nome: 'Refrigerante', valor: 5.0 },
+      { id: 3, nome: 'Batata Frita', valor: 20.0 },
+    ],
+  },
+  {
+    id: 2,
+    mesa: 3,
+    valor: 35.0,
+    itens: [
+      { id: 1, nome: 'Pizza', valor: 25.0 },
+      { id: 2, nome: 'Suco', valor: 5.0 },
+      { id: 3, nome: 'Sobremesa', valor: 5.0 },
+    ],
+  },
+  {
+    id: 3,
+    mesa: 2,
+    valor: 70.0,
+    itens: [
+      { id: 1, nome: 'Frango à Passarinho', valor: 30.0 },
+      { id: 2, nome: 'Cerveja', valor: 10.0 },
+      { id: 3, nome: 'Salada', valor: 15.0 },
+      { id: 4, nome: 'Sobremesa', valor: 15.0 },
+    ],
+  },
+  {
+    id: 4,
+    mesa: 4,
+    valor: 40.0,
+    itens: [
+      { id: 1, nome: 'Sanduíche', valor: 20.0 },
+      { id: 2, nome: 'Refrigerante', valor: 5.0 },
+      { id: 3, nome: 'Batata Frita', valor: 15.0 },
+    ],
+  },
+];
+
 function App() {
   return (
     <>
@@ -18,8 +64,12 @@ function App() {
           <Route path="/exibe-mesa/:id" element={<DetailsMesa />}></Route>
           <Route path="/cozinha" element={<Cozinha />}></Route>
         </Routes>
+        
+        <Pedidos pedidos={pedidos} />
+      
       </Layout>
     </>
+    
   );
 }
 
