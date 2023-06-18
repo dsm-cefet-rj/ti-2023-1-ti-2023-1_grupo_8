dadea4828f903a0bd0e8784fd0a5b1fc3ced5d00
Ricardo <ricardo.fa@gmail.com>
2023-06-18 00:05:20 -0300

    Aumento de Legibilidade e Responsividade em home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,45 +1,51 @@
 import { Link, useLocation } from "react-router-dom";
-import "./home.css"
+import "./home.css";
+
 function Home() {
   const location = useLocation();
 
   return (
-    <>
-
-    <div className="titulo">
+    <div className="container">
+      <div className="titulo">
         <h1 className="appname">TableMaster</h1>
         <h2 className="pagename">Mais praticidade para você e seus clientes</h2>
-    </div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/mesas" className={`btn-green ${location.pathname === "/mesas" ? "active" : "" }`}>
-              Mesas
-            </Link>
-          </li>
-          <li>
-            <Link to="/cozinha" className={`btn-green ${location.pathname === "/cozinha" ? "active" : ""}`}>
-              Cozinha
-            </Link>
-          </li>
-          <li>
-            <Link to="/cardapio" className={`btn-green ${location.pathname === "/cardapio" ? "active" : ""}`}>
-              Cardápio
-            </Link>
-          </li>
-          <li>
-            <Link to="/garcom" className={`btn-green ${location.pathname === "/garcom" ? "active" : ""}`}>
-              Garçom
-            </Link>
-          </li>
-          <li>
-            <Link to="/pedidos-concluidos" className={`btn-green ${location.pathname === "/pedidos-concluidos" ? "active" : ""}`}>
-              Financeiro
-            </Link>
-          </li>
-        </ul>
+      </div>
+      <nav className="d-flex flex-wrap justify-content-center">
+        <Link
+          to="/mesas"
+          className={`btn-green ${location.pathname === "/mesas" ? "active" : ""}`}
+        >
+          Mesas
+        </Link>
+        <Link
+          to="/cozinha"
+          className={`btn-green ${location.pathname === "/cozinha" ? "active" : ""}`}
+        >
+          Cozinha
+        </Link>
+        <Link
+          to="/cardapio"
+          className={`btn-green ${location.pathname === "/cardapio" ? "active" : ""}`}
+        >
+          Cardápio
+        </Link>
+        <Link
+          to="/garcom"
+          className={`btn-green ${location.pathname === "/garcom" ? "active" : ""}`}
+        >
+          Garçom
+        </Link>
+        <Link
+          to="/pedidos-concluidos"
+          className={`btn-green ${location.pathname === "/pedidos-concluidos" ? "active" : ""}`}
+        >
+          Financeiro
+        </Link>
       </nav>
-    </>
+    </div>
   );
 }
+
 export default Home;
+
+
